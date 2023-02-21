9e13d6575b0a91af160c45653ee6ba78abf06252
BorislavGajic <borawr22@gmail.com>
2023-02-21 17:51:39 +0100

    init proj

---
+++ Backend/src/test/java/com/pravnainfo/pravnainformatika/PravnainformatikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravnainfo.pravnainformatika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PravnainformatikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
